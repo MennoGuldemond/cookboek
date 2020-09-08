3f1c36e53b0f62e68bf86f6f6188c1aadf0aee68
Menno Guldemond <mennoguldemond@live.nl>
2020-09-08 21:50:17 +0200

    Added basic sharing

--- src/app/components/recipe-detail/recipe-detail.component.ts
+++ src/app/components/recipe-detail/recipe-detail.component.ts
@@ -2,34 +2,45 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 
-import { RecipeService } from '../../services';
+import { RecipeService, DeviceService } from '../../services';
 import { Recipe } from '../../models';
-
 
 @Component({
   selector: 'cobo-recipe-detail',
   templateUrl: './recipe-detail.component.html',
-  styleUrls: ['./recipe-detail.component.scss']
+  styleUrls: ['./recipe-detail.component.scss'],
 })
 export class RecipeDetailComponent implements OnInit {
-
   recipe$: Observable<Recipe>;
 
   constructor(
+    public deviceService: DeviceService,
     private route: ActivatedRoute,
     private recipeService: RecipeService,
-    private router: Router) { }
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
+    this.route.params.subscribe((params) => {
       if (params.id) {
         this.recipe$ = this.recipeService.getById(params.id);
       }
-   });
+    });
   }
 
   onEditClick(id: string): void {
     this.router.navigate([`recipe-edit/${id}`]);
   }
 
+  onClickShare(title: string): void {
+    try {
+      window.navigator.share({
+        title: `${title} delen`,
+        url: window.location.href
+      })
+    } catch (error) {
+      console.log('This browser does not support the share function.')
+    }
+
+  }
 }
