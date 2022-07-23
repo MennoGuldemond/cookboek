28240dcc6e2f41d4e63e5ccc3a444acf1bd9d986
Menno Guldemond <mennoguldemond@live.nl>
2022-07-23 09:40:56 +0200

    fix circular dependency

--- src/app/components/recipe-detail/recipe-detail.component.ts
+++ src/app/components/recipe-detail/recipe-detail.component.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
 
 import { RecipeService, DeviceService, AuthService, PhotoService } from '../../services';
 import { Recipe } from '../../models';
-import { YesNoDialogComponent } from '../../components';
+import { YesNoDialogComponent } from '../yes-no-dialog/yes-no-dialog.component';
 
 @Component({
   selector: 'cobo-recipe-detail',
@@ -14,7 +14,6 @@ import { YesNoDialogComponent } from '../../components';
   styleUrls: ['./recipe-detail.component.scss'],
 })
 export class RecipeDetailComponent implements OnInit {
-
   recipe$: Observable<Recipe>;
 
   constructor(
@@ -25,8 +24,8 @@ export class RecipeDetailComponent implements OnInit {
     private photoService: PhotoService,
     private router: Router,
     private snackBar: MatSnackBar,
-    private dialog: MatDialog,
-  ) { }
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
@@ -41,32 +40,35 @@ export class RecipeDetailComponent implements OnInit {
   }
 
   onDeleteClick(recipe: Recipe): void {
-    this.dialog.open(YesNoDialogComponent, {
-      data: {
-        title: 'Recept verwijderen',
-        text: 'Weet je zeker dat je het recept wilt verijderen?'
-      }
-    }).afterClosed().subscribe(clickedYes => {
-      if (clickedYes) {
-        this.recipeService.delete(recipe).subscribe(succeeded => {
-          if (succeeded) {
-            this.photoService.delete(recipe).subscribe(x => {
-              this.snackBar.open('Het recept is verwijderd', 'Oke', { duration: 3000 });
-              this.router.navigate(['recipes']);
-            });         
-          } else {
-            this.snackBar.open('Verwijderen mislukt', 'Oke', { duration: 3000 });
-          }
-        });
-      }
-    });
+    this.dialog
+      .open(YesNoDialogComponent, {
+        data: {
+          title: 'Recept verwijderen',
+          text: 'Weet je zeker dat je het recept wilt verijderen?',
+        },
+      })
+      .afterClosed()
+      .subscribe((clickedYes) => {
+        if (clickedYes) {
+          this.recipeService.delete(recipe).subscribe((succeeded) => {
+            if (succeeded) {
+              this.photoService.delete(recipe).subscribe((x) => {
+                this.snackBar.open('Het recept is verwijderd', 'Oke', { duration: 3000 });
+                this.router.navigate(['recipes']);
+              });
+            } else {
+              this.snackBar.open('Verwijderen mislukt', 'Oke', { duration: 3000 });
+            }
+          });
+        }
+      });
   }
 
   onClickShare(title: string): void {
     try {
       window.navigator.share({
         title: `${title} delen`,
-        url: window.location.href
+        url: window.location.href,
       });
     } catch (error) {
       console.error('This browser does not support the share function.');
@@ -79,16 +81,15 @@ export class RecipeDetailComponent implements OnInit {
 
   onLikeClick(event: MouseEvent, recipe: Recipe): void {
     event.stopPropagation();
-    this.auth.user$.subscribe(user => {
+    this.auth.user$.subscribe((user) => {
       this.recipeService.addLike(recipe, user.uid);
     });
   }
 
   onUnlikeClick(event: MouseEvent, recipe: Recipe): void {
     event.stopPropagation();
-    this.auth.user$.subscribe(user => {
+    this.auth.user$.subscribe((user) => {
       this.recipeService.removeLike(recipe, user.uid);
     });
   }
-
 }
