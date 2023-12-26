7eac1948659127c57bc25a84c3b4871ddc4e2779
Menno Guldemond <mennoguldemond@live.nl>
2023-12-26 14:56:12 +0100

    version 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,12 @@
 import packageJson from '../../package.json';
 
 export const environment = {
+  api: {
+    url: 'http://localhost:3000',
+  },
+  google: {
+    clientId: '135413077856-lrkj5n01tgl3bcbl0hpneg73s8blinh3.apps.googleusercontent.com',
+  },
   production: true,
   version: packageJson.version,
 };
