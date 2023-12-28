9c11fa4ec1a959b96400bbc904f89f675f6924bd
Menno Guldemond <mennoguldemond@live.nl>
2023-12-28 11:37:17 +0100

    prod to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import packageJson from '../../package.json';
 
 export const environment = {
   api: {
-    url: 'http://cookboek.hopto.org:3000',
+    url: 'https://cookboek.hopto.org:3000',
   },
   google: {
     clientId: '135413077856-lrkj5n01tgl3bcbl0hpneg73s8blinh3.apps.googleusercontent.com',
