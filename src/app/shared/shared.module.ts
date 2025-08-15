9ffd76575d146afadcfe34af29449bb2f5c7026c
Menno Guldemond <mennoguldemond@live.nl>
2025-08-15 16:05:35 +0200

    update to angular 18

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
 import { MaterialModule } from './material.module';
 import { ChipAutocompleteComponent } from './components';
 
-@NgModule({
-  imports: [CommonModule, HttpClientModule, MaterialModule, ReactiveFormsModule],
-  declarations: [ChipAutocompleteComponent],
-  exports: [CommonModule, ReactiveFormsModule, MaterialModule, ChipAutocompleteComponent],
-  providers: [{ provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher }],
-})
+@NgModule({ declarations: [ChipAutocompleteComponent],
+    exports: [CommonModule, ReactiveFormsModule, MaterialModule, ChipAutocompleteComponent], imports: [CommonModule, MaterialModule, ReactiveFormsModule], providers: [{ provide: ErrorStateMatcher, useClass: ShowOnDirtyErrorStateMatcher }, provideHttpClient(withInterceptorsFromDi())] })
 export class SharedModule {}
