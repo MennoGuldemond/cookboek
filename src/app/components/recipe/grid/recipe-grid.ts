98c17cebdf060a38eaeac13d1bbb62386894e95d
Menno Guldemond <mennoguldemond@live.nl>
2025-08-17 20:46:49 +0200

    desktop add button

--- src/app/components/recipe/grid/recipe-grid.ts
+++ src/app/components/recipe/grid/recipe-grid.ts
@@ -1,19 +1,22 @@
 import { Component, inject, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { RecipeInfo } from '@app/models';
 import { RecipeCard } from '../card/recipe-card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { BrowserUtilService } from '@app/services';
 
 @Component({
   selector: 'cobo-recipe-grid',
   templateUrl: './recipe-grid.html',
   styleUrls: ['./recipe-grid.scss'],
-  imports: [RecipeCard, MatButtonModule, MatIconModule],
+  imports: [CommonModule, RecipeCard, MatButtonModule, MatIconModule],
 })
 export class RecipeGrid {
   @Input() recipes: RecipeInfo[];
 
+  browserUtils = inject(BrowserUtilService);
   private router = inject(Router);
 
   onRecipeClick(recipe: RecipeInfo) {
