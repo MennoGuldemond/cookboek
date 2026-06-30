a19b0773c3f7288e9c3e9d1742b34d0c41cbae93
Menno Guldemond <mennoguldemond@live.nl>
2026-06-30 21:44:33 +0200

    remove redundant slash

--- src/environments/environment.prd.ts
+++ src/environments/environment.prd.ts
@@ -2,7 +2,7 @@ import packageJson from '../../package.json';
 
 export const environment = {
   api: {
-    url: 'https://cookboek-prd-api.azurewebsites.net/',
+    url: 'https://cookboek-prd-api.azurewebsites.net',
   },
   google: {
     clientId: '135413077856-lrkj5n01tgl3bcbl0hpneg73s8blinh3.apps.googleusercontent.com',
--- src/environments/environment.tst.ts
+++ src/environments/environment.tst.ts
@@ -3,7 +3,7 @@ import packageJson from '../../package.json';
 export const environment = {
   api: {
     // Set the tst API base URL for test deployments.
-    url: 'https://cookboek-tst-api.azurewebsites.net/',
+    url: 'https://cookboek-tst-api.azurewebsites.net',
   },
   google: {
     clientId: '135413077856-lrkj5n01tgl3bcbl0hpneg73s8blinh3.apps.googleusercontent.com',
